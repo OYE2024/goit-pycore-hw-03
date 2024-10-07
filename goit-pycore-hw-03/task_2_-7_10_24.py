d71a831c10458c0c63d82a02c8e434c9551c3cab
Oleksandr Yesypenko <alex.iesypenko@gmail.com>
2024-10-07 22:39:33 +0100

    was added an exception ValueError if quantity parameter more than unique numbers from range

--- goit-pycore-hw-03/task_2_-7_10_24.py
+++ goit-pycore-hw-03/task_2_-7_10_24.py
@@ -8,11 +8,15 @@ from random import sample
 
 
 def get_numbers_ticket(min, max, quantity) -> list:
-    if min < 1 or max > 1000 or 0 < quantity > 1000:
-        return []
-    else:
-        # max+1 тому що діапазон формує діапазон чисел не включаючи максимальне значення.
-        return sorted(sample(range(min, max+1), quantity))
+    try:
+        if min < 1 or max > 1000 or max < quantity < min:
+            return []
+        else:
+            # max+1 тому що діапазон формує діапазон чисел не включаючи максимальне значення.
+            return sorted(sample(range(min, max+1), quantity))
+    except ValueError:
+        print(f"Error. Quantity must be bigger than {
+              min} and less then {max}.")
 
 
-print(get_numbers_ticket(10, 300, 6))
+print(get_numbers_ticket(10, 300, 292))
